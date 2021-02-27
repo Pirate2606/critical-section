8d5e8751ee0e0d94d061b162dad02a34062aee8a
Pirate2606 <adityanaitan@gmail.com>
2021-02-27 12:51:33 +0530

    created navbar and footer

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def home():
     return render_template('home.html')
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(403)
+def restricted(error):
+    return render_template('403.html'), 403
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
